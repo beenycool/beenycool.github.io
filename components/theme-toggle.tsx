030d9ad12d0ef9ea0cae81fe0e7931cc62356b1a
beenycool <yaseensaoud@outlook.com>
2025-05-08 10:27:59 +0000

    refractor for design and performance

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -22,11 +22,11 @@ export function ThemeToggle() {
           >
             <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
+            <span className="sr-only">Switch theme</span>
           </Button>
         </TooltipTrigger>
         <TooltipContent>
-          <p>Toggle theme</p>
+          <p>Light/Dark</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
