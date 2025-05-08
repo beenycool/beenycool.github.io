ad26449d00f77faa4c1cafac28ff492f33d2076a
beenycool <yaseensaoud@outlook.com>
2025-05-08 09:27:44 +0000

    replace for  dynamic loading to fix a bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import AIMarker from "@/app/aimarker";
 import { Header } from "@/components/header";
+import ClientWrapper from './client-wrapper';
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className="container flex min-h-[calc(100vh-3.5rem)] flex-col items-center justify-center py-8">
-        <AIMarker />
+        <ClientWrapper />
       </main>
     </>
   );
