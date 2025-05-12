1c068a58c0a3128b18adc634ccd29a31fe9380d1
Beeny <129210955+beenycool@users.noreply.github.com>
2025-05-12 21:22:33 +0100

    fixes

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
-const repoName = 'beenycool.github.io'; // <-- Change this to your repo name if different
-
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true
   },
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: '',
+  assetPrefix: '',
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001',
   },
